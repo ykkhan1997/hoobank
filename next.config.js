dfaa64f472b93decccf2cca8c03aa075f5e62a3b
Harry <ykkhan1997@gmail.com>
2023-03-18 10:26:20 +0300

    ignore some rules of eslint

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
+
+module.exports = {
+    reactStrictMode: true,
+    eslint: {
+      ignoreDuringBuilds: true,
+    },
 }
-module.exports = nextConfig
